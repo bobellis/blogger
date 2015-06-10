37875275c5e7e08aad86faa4f7aba6b9ddc096d9
bobellis <bobellis.tx@gmail.com>
2015-06-10 10:56:22 -0700

    update blogger app

--- router.js
+++ router.js
@@ -1,9 +1,15 @@
 Blogger.Router.map(function(){
   this.resource('posts', {path: '/'});
+  this.resource('post', {path: 'posts/:post_id'}, function(){
+    this.resource('new-comment');
+  });
+
   this.resource('about');
   this.resource('contact', function(){
     this.resource('phone');
     this.resource('email');
   });
+  this.resource('recent-comments');
+  this.resource('new-post');
 
 });
